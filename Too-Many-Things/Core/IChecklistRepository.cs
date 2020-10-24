dd9b1022e5ae8b89d69b0425b15e0dc602b3e719
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-10-24 06:38:00 +0200

    Implement Checklist repository pattern

---
+++ Too-Many-Things/Core/IChecklistRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Too_Many_Things.Models;
+
+namespace Too_Many_Things.Core
+{
+    public interface IChecklistRepository
+    {
+        Checklist GetChecklistByID(int id);
+        Checklist GetChecklistByName(string name);
+        DbSet<Checklist> GetChecklistDbSet();
+        public void CompleteUnitOfWork();
+    }
+}

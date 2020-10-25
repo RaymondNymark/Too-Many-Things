102d95b0f62b2d4543016fa6fd4df88a57b03b0c
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-10-25 06:55:15 +0100

    Create simplistic IOC container config class

---
+++ Too-Many-Things/ContainerConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Too_Many_Things.Core;
+using Too_Many_Things.Models;
+
+namespace Too_Many_Things
+{
+    public static class ContainerConfig
+    {
+        // Simple IOC container configure method.
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ChecklistService>().As<IChecklistService>();
+
+            //builder.RegisterType<TooManyThingsContext>().
+            return builder.Build();
+        }
+    }
+}

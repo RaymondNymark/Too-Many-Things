87becfaeba50600ab7614cb3c4c0eae0f92ee6f5
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-10-26 07:41:08 +0100

    Massively update ChecklistService to implement DbContextScope pattern for more flexibility
    Instead of using injected DbContext, according to this article and various articles referencing it, I should be using a DbContextScope instead to enable async workflows in the application.

--- Too-Many-Things/Core/ChecklistService.cs
+++ Too-Many-Things/Core/ChecklistService.cs
@@ -1,11 +1,12 @@
-﻿using EntityFramework.DbContextScope.Interfaces;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Too_Many_Things.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCore.DbContextScope;
 
 namespace Too_Many_Things.Core
 {
@@ -17,44 +18,35 @@ namespace Too_Many_Things.Core
          * application is low and thus having a single Checklist service class
          * seems appropriate enough.
          */
-
-        //private readonly IDbContextScopeFactory _dbContextScopeFactory;
-        //private readonly IAmbientDbContextLocator _ambientDbContextLocator;
-
-        //private TooManyThingsContext _dbContext
-        //{
-        //    get
-        //    {
-        //        var dbContext = _ambientDbContextLocator.Get<TooManyThingsContext>();
-        //    }
-        //}
-
-        //public ChecklistService(IDbContextScopeFactory dbContextScopeFactory, IAmbientDbContextLocator ambientDbContextLocator)
-        //{
-        //    if (dbContextScopeFactory == null) throw new ArgumentNullException("dbContextScopeFactory");
-        //    if (ambientDbContextLocator == null) throw new ArgumentNullException("ambientDbContextLocator");
-        //    _dbContextScopeFactory = dbContextScopeFactory;
-        //    _ambientDbContextLocator = ambientDbContextLocator;
-        //}
-        private readonly TooManyThingsContext _dbContext;
         private readonly ILogger _logger;
-
-        public ChecklistService(TooManyThingsContext dbContext, ILogger logger)
+        private readonly IAmbientDbContextLocator _ambientDbContextLocator;
+        private readonly IDbContextScopeFactory _dbContextScopeFactory;
+        private TooManyThingsContext DbContext
         {
-            _dbContext = dbContext ?? throw new ArgumentNullException("dbContext");
-            _logger = logger ?? throw new ArgumentNullException("logger");
+            get => _ambientDbContextLocator.Get<TooManyThingsContext>() ?? throw new ArgumentNullException("_ambientDbContextLocator");
         }
 
-        #region Internal Read operations
-        private Checklist Get(int checklistID) => _dbContext.Set<Checklist>().FirstOrDefault(i => i.ChecklistId == checklistID);
-        private Checklist Get(string checklistName) => _dbContext.Set<Checklist>().FirstOrDefault(i => i.Name == checklistName);
-        //private Task<Checklist> GetAsync(int checklistID) => _dbContext.Set<Checklist>().FindAsync(checklistID);
+        public ChecklistService(ILogger logger, IDbContextScopeFactory dbContextScopeFactory, IAmbientDbContextLocator ambientDbContextLocator)
+        {
+            _logger = logger ?? throw new ArgumentNullException("logger");
+            _dbContextScopeFactory = dbContextScopeFactory ?? throw new ArgumentNullException("dbContextScopeFactory");
+            _ambientDbContextLocator = ambientDbContextLocator ?? throw new ArgumentNullException("ambientDbContextLocator");
+        }
+
+        #region Traditional 'repostory' services.
+        private Checklist Get(int checklistID)
+        {
+            return DbContext.Checklist.Find(checklistID);
+        }
+        private ValueTask<Checklist> GetAsync(int checklistID)
+        {
+            return DbContext.Checklist.FindAsync(checklistID);
+        }
+
         #endregion
 
 
-
         #region Services
-
         // TODO : Check data validation for all of these.
 
         public void CreateChecklist(Checklist checklist)
@@ -88,8 +80,21 @@ namespace Too_Many_Things.Core
         }
 
         // TODO!! : Implement methods to re-order items.!!
-
         #endregion
+
+
+        #region Updated Services
+        public void TestService(int checklistID)
+        {
+            using (var dbContextScope = _dbContextScopeFactory.Create())
+            {
+                var checklist = Get(checklistID);
+                // [...}
+                dbContextScope.SaveChanges();
+            }
+        }
+        #endregion
+
 
         #region Data validation
         #endregion

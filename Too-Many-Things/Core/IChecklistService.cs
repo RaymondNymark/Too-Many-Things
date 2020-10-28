1491760a614e2dc63cd3c4fd1ac2480f87d09dce
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-10-28 06:20:58 +0100

    Update ChecklistService interface

--- Too-Many-Things/Core/IChecklistService.cs
+++ Too-Many-Things/Core/IChecklistService.cs
@@ -1,15 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Too_Many_Things.Models;
 
 namespace Too_Many_Things.Core
 {
     public interface IChecklistService
     {
+        void Get(int checklistID);
+        Task GetAsync(int checklistID);
+
+
         void CreateChecklist(Checklist checklist);
         Task CreateChecklistAsync(Checklist checklist);
-        void RemoveChecklist(int checklistID);
-        Task RemoveChecklistAsync(int checklistID);
+
+
+        void CreateDefaultChecklist();
+        Task CreateDefaultChecklistAsync();
+
+
+        void DeleteChecklist(int checklistID);
+        Task DeleteChecklistAsync(int checklistID);
+
+        void DeleteChecklist(IList<int> listOfChecklistIDs);
+        Task DeleteChecklistAsync(IList<int> listOfChecklistIDs);
+
+
+        void PermanentlyDeleteChecklist(int checklistID);
+        Task PermanentlyDeleteChecklistAsync(int checklistID);
+
+
         void RenameChecklist(int checklistID, string newName);
         Task RenameChecklistAsync(int checklistID, string newName);
+
+
+        void GetLocalView();
+        Task GetLocalViewAsync();
     }
 }

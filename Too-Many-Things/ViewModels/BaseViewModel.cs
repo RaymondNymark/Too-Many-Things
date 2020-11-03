dd80f88c2df487aa1ac4cbca24611d57f317185f
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-03 10:23:47 +0100

    Create BaseViewModel.cs class for ViewModels to inherit from

---
+++ Too-Many-Things/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Too_Many_Things.ViewModels
+{
+        /*
+         * Base class all ViewModels shall inherit from.  Implements clean,
+         * quick, and easy property change handling for properties that belong
+         * to individual viewModels
+         */
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+    }
+}

1908a9f300d612967d90c5806fc52b80e0b00458
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-03 11:12:39 +0100

    Create ViewModelController.cs

---
+++ Too-Many-Things/ViewModels/ViewModelController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Too_Many_Things.Services;
+using Too_Many_Things.Models;
+
+
+namespace Too_Many_Things.ViewModels
+{
+    public class ViewModelController : ViewModelBase
+    {
+        #region Initialization
+        private IChecklistService _checklistService;
+        private ViewModelBase _selectedViewModel;
+
+
+        public ViewModelController(IChecklistService checklistService, ViewModelBase selectedViewModel)
+        {
+            _checklistService = checklistService;
+            _selectedViewModel = selectedViewModel;
+        }
+        #endregion
+
+
+        #region View Controls
+        public ViewModelBase SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set => SetProperty(ref _selectedViewModel, value);
+        }
+        #endregion
+
+
+    }
+}

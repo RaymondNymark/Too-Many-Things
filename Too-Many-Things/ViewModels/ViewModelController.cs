18a2d79579c99150d75860f2b2a3bfaedc3b47f1
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-04 07:21:29 +0100

    Implement delegate controll to ViewModelController.cs

--- Too-Many-Things/ViewModels/ViewModelController.cs
+++ Too-Many-Things/ViewModels/ViewModelController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Too_Many_Things.Services;
 using Too_Many_Things.Models;
-
+using System.Windows.Input;
 
 namespace Too_Many_Things.ViewModels
 {
@@ -31,5 +31,31 @@ namespace Too_Many_Things.ViewModels
         #endregion
 
 
+        #region Delegate + ICommands
+        /// <summary>
+        /// Simplistic delegate command implementation
+        /// </summary>
+        public class DelegateCommand : ICommand
+        {
+            public Action CommandAction { get; set; }
+            public Func<bool> CanExecuteFunc { get; set; }
+
+            public void Execute(object parameter)
+            {
+                CommandAction();
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return CanExecuteFunc == null || CanExecuteFunc();
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+        }
+        #endregion
     }
 }

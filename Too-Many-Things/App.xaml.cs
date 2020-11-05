47a47730b11870d392322048931f7d6a362392cf
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-05 06:46:15 +0100

    Implement bootstrapper to App.xaml + App.xaml.cs to configure autofac

--- Too-Many-Things/App.xaml.cs
+++ Too-Many-Things/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
+using Too_Many_Things.Views;
+using Too_Many_Things.ViewModels;
+using Too_Many_Things.Services;
 
 namespace Too_Many_Things
 {
@@ -13,5 +17,22 @@ namespace Too_Many_Things
     /// </summary>
     public partial class App : Application
     {
+        private IChecklistService _checklistService;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                _checklistService = scope.Resolve<IChecklistService>();
+                // TODO1: Resolve DbContextScopeFactory.
+                // TODO2: Implement startup constructors. 
+
+                var window = new OuterView();
+                window.DataContext = new ViewModelController(_checklistService, new OuterViewModel());
+            }
+        }
     }
 }

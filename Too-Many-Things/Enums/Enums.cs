bc240f32647790d6f9de7ba4303622217337e4ab
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-13 10:47:11 +0100

    Create Enums.cs for SortOrder enum

---
+++ Too-Many-Things/Enums/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Too_Many_Things.Enums
+{
+    public static class Enums
+    {
+        public enum SortOrder
+        {
+            Default,
+            AscendingOrder,
+            DescendingOrder
+        }
+    }
+}

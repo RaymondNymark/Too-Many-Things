4eab42a9f3611286fd8e0a39ac3d509fe0ea7646
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-15 06:23:15 +0100

    Add GetSampleChecklist() to ChecklistServiceTests.cs

--- Too-Many-Things.Tests/Services/ChecklistServiceTests.cs
+++ Too-Many-Things.Tests/Services/ChecklistServiceTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 using Moq;
 using Too_Many_Things.Models;
 using Autofac.Extras.Moq;
+using System.Collections.ObjectModel;
 
 namespace Too_Many_Things.Tests
 {
@@ -21,6 +22,25 @@ namespace Too_Many_Things.Tests
             var actual = ChecklistService.ValidateChecklist(input);
 
             Assert.False(actual);
+        }
+
+        public ObservableCollection<Checklist> GetSampleChecklistList()
+        {
+            var itemOne = (new Checklist { Name = "Groceries", SortOrder = 1, ChecklistId = 0});
+            var itemTwo = (new Checklist { Name = "Chores", SortOrder = 2, ChecklistId = 1 });
+            var itemThree = (new Checklist { Name = "Travel-bag", SortOrder = 3, ChecklistId = 2 });
+
+            itemOne.Entry.Add(new Entry { Name = "Milk", EntryId = 1, SortOrder = 1, IsChecked = false});
+            itemOne.Entry.Add(new Entry { Name = "Sugar", EntryId = 2, SortOrder = 2, IsChecked = false });
+            itemOne.Entry.Add(new Entry { Name = "Oats", EntryId = 3, SortOrder = 3, IsChecked = false });
+            itemOne.Entry.Add(new Entry { Name = "Dark Chocolate", EntryId = 4, SortOrder = 4, IsChecked = false });
+
+            var list = new ObservableCollection<Checklist>();
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.Add(itemThree);
+
+            return list;
         }
     }
 }

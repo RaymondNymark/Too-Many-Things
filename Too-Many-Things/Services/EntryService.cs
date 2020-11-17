b5e51364619430aad4ec7c3ee1a6e4989bee3518
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-17 07:41:48 +0100

    Update EntryService.cs

--- Too-Many-Things/Services/EntryService.cs
+++ Too-Many-Things/Services/EntryService.cs
@@ -13,6 +13,67 @@ namespace Too_Many_Things.Services
 {
     public partial class ChecklistService : IChecklistService
     {
+        /// <summary>
+        /// Returns an ObservableCollection of entries in a checklist.
+        /// </summary>
+        /// <param name="checklistID">Checklist ID</param>
+        /// <returns></returns>
+        public ObservableCollection<Entry> GetEntriesFromChecklist(int checklistID)
+        {
+            var selected = Get(checklistID);
+            var output = selected.Entry;
 
+            return output;
+        }
+
+        public void AddNewEntry(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewEntryAsync(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNewDefaultEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewDefaultEntryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEntry(int entryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEntryAsync(int entryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PermenentlyDeletEntry(int entryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PermenentlyDeleteEntryAsync(int entryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenameEntry(int entryID, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RenameEntryAsync(int entryID, string newName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

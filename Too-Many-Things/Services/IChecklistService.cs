fcd3b32493c300484b6a0200fd319191c054ee23
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-17 06:14:05 +0100

    Update IChecklistService.cs

--- Too-Many-Things/Services/IChecklistService.cs
+++ Too-Many-Things/Services/IChecklistService.cs
@@ -10,25 +10,30 @@ namespace Too_Many_Things.Services
         Checklist Get(int checklistID);
         Task<Checklist> GetAsync(int checklistID);
 
-
         void AddNewChecklist(Checklist checklist);
         Task AddNewChecklistAsync(Checklist checklist);
-
+        void AddNewEntry(Entry entry);
+        Task AddNewEntryAsync(Entry entry);
 
         void AddNewDefaultChecklist();
         Task AddNewDefaultChecklistAsync();
-
+        void AddNewDefaultEntry();
+        Task AddNewDefaultEntryAsync();
 
         void DeleteChecklist(int checklistID);
         Task DeleteChecklistAsync(int checklistID);
-
+        void DeleteEntry(int entryID);
+        Task DeleteEntryAsync(int entryID);
 
         void PermanentlyDeleteChecklist(int checklistID);
         Task PermanentlyDeleteChecklistAsync(int checklistID);
-
+        void PermenentlyDeletEntry(int entryID);
+        Task PermenentlyDeleteEntryAsync(int entryID);
 
         void RenameChecklist(int checklistID, string newName);
         Task RenameChecklistAsync(int checklistID, string newName);
+        void RenameEntry(int entryID, string newName);
+        Task RenameEntryAsync(int entryID, string newName);
 
         ObservableCollection<Checklist> GetLocalCollectionSource();
     }

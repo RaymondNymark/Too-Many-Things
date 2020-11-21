174fc3bcf3a8173a84bc0a97c4a7a215bb555849
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-21 09:41:58 +0100

    Create Inner and Outer views

---
+++ Too-Many-Things.Wpf/Views/OuterView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Too_Many_Things.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for OuterView.xaml
+    /// </summary>
+    public partial class OuterView : UserControl
+    {
+        public OuterView()
+        {
+            InitializeComponent();
+        }
+    }
+}

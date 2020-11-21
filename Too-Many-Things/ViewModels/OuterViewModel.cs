235beec874fe76a07ee056422ff7fa9520459cac
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-21 09:42:28 +0100

    Update OuterViewModel to comply with ReactiveUI

--- Too-Many-Things/ViewModels/OuterViewModel.cs
+++ Too-Many-Things/ViewModels/OuterViewModel.cs
@@ -5,16 +5,28 @@ using Too_Many_Things.Services;
 using Too_Many_Things.Models;
 using Too_Many_Things.ViewModels;
 using System.Collections.ObjectModel;
+using ReactiveUI;
 
 namespace Too_Many_Things.ViewModels
 {
-    public class OuterViewModel : ViewModelBase
+    public interface IOuterViewModel : IRoutableViewModel
     {
+
+    }
+    public class OuterViewModel : ReactiveObject, IOuterViewModel
+    {
+        public string UrlPathSegment
+        {
+            get => "outer";
+        }
+        public IScreen HostScreen { get; protected set; }
+
         private IChecklistService _checklistService;
         private Checklist _selectedChecklist;
 
-        public OuterViewModel(IChecklistService checklistService)
+        public OuterViewModel(IScreen hostScreen, IChecklistService checklistService)
         {
+            HostScreen = hostScreen;
             _checklistService = checklistService;
         }
 
@@ -24,7 +36,7 @@ namespace Too_Many_Things.ViewModels
         public Checklist SelectedChecklist
         {
             get => _selectedChecklist;
-            set => SetProperty(ref _selectedChecklist, value);
+            set => this.RaiseAndSetIfChanged(ref _selectedChecklist, value);
         }
         #endregion
     }

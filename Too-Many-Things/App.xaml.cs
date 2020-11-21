24d25e6a0d27f21d904ef609c1a062692425451f
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-21 09:36:34 +0100

    Remove all references to Autofac from the solution in favor of ReactiveUI Splat for DI.
    Original plan was to use Autofac for IoC, but due to unexpected surprises while working with ReactiveUI for the first time, I’ve abandoned the original plan and instead will be using ReactiveUI’s native Splat instead.
    
    ReactiveUI provides documentation on how to adapt its framework to be usable with Autofac, but the documentation is at least four years out of date, as it uses parts of Autofac that have been marked as obsolete over four years ago.
    
    Due to the scope of this application, manually replacing Splat for Autofac without any proper guidance is probably not a very good time investment as it wouldn’t really do anything for the application that Splat isn’t capable of, and there’s probably something to gain by using ReactiveUI’s favored IoC implementation. :)

--- Too-Many-Things/App.xaml.cs
+++ Too-Many-Things/App.xaml.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using Autofac;
 using Too_Many_Things.Views;
 using Too_Many_Things.ViewModels;
 using Too_Many_Things.Services;
@@ -17,22 +16,6 @@ namespace Too_Many_Things
     /// </summary>
     public partial class App : Application
     {
-        private IChecklistService _checklistService;
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            var container = ContainerConfig.Configure();
-
-            using (var scope = container.BeginLifetimeScope())
-            {
-                _checklistService = scope.Resolve<IChecklistService>();
-
-                var window = new OuterView();
-                window.DataContext = new ViewModelController(_checklistService, new OuterViewModel(_checklistService));
-
-                window.Show();
-            }
-        }
     }
 }

1c821af636fe1787a9b598e3d1d1c7926f107b7e
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-21 09:18:39 +0100

    Create ReactiveUI suited InnerViewModel.cs

--- Too-Many-Things/ViewModels/InnerViewModel.cs
+++ Too-Many-Things/ViewModels/InnerViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Too_Many_Things.Services;
+using Too_Many_Things.Models;
+using Too_Many_Things.ViewModels;
+using System.Collections.ObjectModel;
+using ReactiveUI;
 
 namespace Too_Many_Things.ViewModels
 {
-    class InnerViewModel
+    public interface IInnerViewModel : IRoutableViewModel
     {
+
+    }
+    public class InnerViewModel : ReactiveObject, IInnerViewModel
+    {
+        public string UrlPathSegment
+        {
+            get => "inner";
+        }
+        public IScreen HostScreen { get; protected set; }
+        private IChecklistService _checklistService;
+
+        public InnerViewModel(IScreen hostScreen, IChecklistService checklistService)
+        {
+            HostScreen = hostScreen;
+            _checklistService = checklistService;
+        }
     }
 }

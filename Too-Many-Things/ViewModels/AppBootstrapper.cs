37a3e678004b1fe1a34b2b5d233f12ccf0f81fe9
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-21 09:04:30 +0100

    Remove legacy code from AppBootstrapper.cs and fix a typo on line 32.

--- Too-Many-Things/ViewModels/AppBootstrapper.cs
+++ Too-Many-Things/ViewModels/AppBootstrapper.cs
@@ -29,7 +29,7 @@ namespace Too_Many_Things.ViewModels
         }
 
 
-        // Registers the dependancies all in one place. 
+        // Registers the dependencies all in one place. 
         private void RegisterComponets(IMutableDependencyResolver dependencyResolver)
         {   // Views + ViewModels
             dependencyResolver.Register(() => new OuterView(), typeof(IViewFor<OuterViewModel>));
@@ -43,104 +43,5 @@ namespace Too_Many_Things.ViewModels
             var ambientDbContextLocator = Locator.Current.GetService<IAmbientDbContextLocator>();
             dependencyResolver.Register(() => new ChecklistService(dbContextScopeFactory, ambientDbContextLocator), typeof(IChecklistService));
         }
-
-        //private IChecklistService _checklistService;
-        //public RoutingState Router { get; private set; }
-
-        //public AppBootstrapper(IContainer container = null, RoutingState router = null)
-        //{
-        //    Router = router ?? new RoutingState();
-        //    container = container ?? ContainerConfig.Configure();
-
-        //    var autofacResolver = container.Resolve<AutofacDependencyResolver>();
-
-        //    autofacResolver.SetLifetimeScope(container);
-
-        //    using (var scope = container.BeginLifetimeScope())
-        //    {
-        //        _checklistService = scope.Resolve<IChecklistService>();
-
-        //        // Navigate to the opening page of this application.
-        //        Router.Navigate.Execute(new OuterViewModel(_checklistService));
-        //    }
-        //}
-    }
-
-    //public class AutofacDependencyResolver : IMutableDependencyResolver
-    //{
-    //    private readonly IContainer _container;
-
-    //    public AutofacDependencyResolver(IContainer container)
-    //    {
-    //        _container = container;
-    //    }
-
-    //    public object GetService(Type serviceType, string contract = null)
-    //    {
-    //        try
-    //        {
-    //            return string.IsNullOrEmpty(contract)
-    //                ? _container.Resolve(serviceType)
-    //                : _container.ResolveNamed(contract, serviceType);
-    //        }
-    //        catch(DependencyResolutionException)
-    //        {
-    //            return null;
-    //        }
-    //    }
-
-    //    public IEnumerable<object> GetServices(Type serviceType, string contract = null)
-    //    {
-    //        try
-    //        {
-    //            var enumerableType = typeof(IEnumerable<>).MakeGenericType(serviceType);
-    //            object instance = string.IsNullOrEmpty(contract)
-    //                ? _container.Resolve(enumerableType)
-    //                : _container.ResolveNamed(contract, enumerableType);
-    //            return ((IEnumerable)instance).Cast<object>();
-    //        }
-    //        catch (DependencyResolutionException)
-    //        {
-    //            return null;
-    //        }
-    //    }
-
-    //    public void Register(Func<object> factory, Type serviceType, string contract = null)
-    //    {
-    //        var builder = new ContainerBuilder();
-    //        if (string.IsNullOrEmpty(contract))
-    //        {
-    //            builder.Register(x => factory()).As(serviceType).AsImplementedInterfaces();
-    //        }
-    //        else
-    //        {
-    //            builder.Register(x => factory()).Named(contract, serviceType).AsImplementedInterfaces();
-    //        }
-    //    }
-
-    //    public IDisposable ServiceRegistrationCallback(Type serviceType, string contract, Action<IDisposable> callback)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Dispose()
-    //    {
-    //        _container.Dispose();
-    //    }
-
-    //    public bool HasRegistration(Type serviceType, string contract = null)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void UnregisterCurrent(Type serviceType, string contract = null)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void UnregisterAll(Type serviceType, string contract = null)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
     }
 }

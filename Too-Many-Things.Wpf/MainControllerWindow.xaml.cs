443ea1447b90dde32c762d04c1b08ff6c3cf22c6
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-30 09:29:47 +0100

    Create MainControllerWindow.xaml to act as a mainWindow

---
+++ Too-Many-Things.Wpf/MainControllerWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Too_Many_Things.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainControllerWindow.xaml
+    /// </summary>
+    public partial class MainControllerWindow : ReactiveWindow<AppBootstrapper>
+    {
+        public AppBootstrapper AppBootStrapper { get; protected set; }
+
+        public MainControllerWindow()
+        {
+            InitializeComponent();
+            AppBootStrapper = new AppBootstrapper();
+            // TODO: Compile app to fix this.
+            ViewModel = AppBootStrapper;
+        }
+    }
+}

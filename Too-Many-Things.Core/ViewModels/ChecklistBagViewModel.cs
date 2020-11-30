4c0ba3f1c790d31374cb8eebc6b4e7694267a98b
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-30 13:22:22 +0100

    Create ChecklistBagVM

---
+++ Too-Many-Things.Core/ViewModels/ChecklistBagViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Too_Many_Things.Core.Services;
+using Too_Many_Things.Core.Models;
+using Too_Many_Things.Core.ViewModels;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using Splat;
+
+namespace Too_Many_Things.Core.ViewModels
+{
+    public class ChecklistBagViewModel : ReactiveObject, IRoutableViewModel
+    {
+        public string UrlPathSegment => "ChecklistBag";
+        public IScreen HostScreen { get; }
+        private IChecklistService _checklistService;
+
+        public ChecklistBagViewModel(IChecklistService checklistService = null)
+        {
+            _checklistService = checklistService ?? Locator.Current.GetService<IChecklistService>();
+        }
+    }
+}

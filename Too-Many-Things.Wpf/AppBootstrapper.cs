b8bde47289616d6c1785db5bbd68b0b44924499f
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-30 12:37:07 +0100

    Reduce amount of work AppBootstrapper has to do

--- Too-Many-Things.Wpf/AppBootstrapper.cs
+++ Too-Many-Things.Wpf/AppBootstrapper.cs
@@ -1,39 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ReactiveUI;
+﻿using ReactiveUI;
 using Too_Many_Things.Core.Services;
 using Splat;
-using System.Collections;
-using System.Linq;
 using Too_Many_Things.Wpf.Views;
 using EntityFrameworkCore.DbContextScope;
 using Too_Many_Things.Core.ViewModels;
 
 namespace Too_Many_Things.Wpf
 {
-    public class AppBootstrapper : ReactiveObject, IScreen
+    public class AppBootstrapper
     {
-        public RoutingState Router { get; private set; }
         private IMutableDependencyResolver _mutableDependencyResolver { get; set; }
 
-        public AppBootstrapper(IMutableDependencyResolver dependencyResolver = null, RoutingState router = null)
+        public AppBootstrapper(IMutableDependencyResolver dependencyResolver = null)
         {
-            Router = router ?? new RoutingState();
             _mutableDependencyResolver = dependencyResolver ?? Locator.CurrentMutable;
-
-            // Should *probably* put any other startup tasks here:
             RegisterComponets(_mutableDependencyResolver);
-
-            Router.Navigate.Execute(new OuterViewModel(this, Locator.Current.GetService<IChecklistService>()));
         }
-
 
         // Registers the dependencies all in one place. 
         private void RegisterComponets(IMutableDependencyResolver dependencyResolver)
         {   // Views + ViewModels
             dependencyResolver.Register(() => new OuterView(), typeof(IViewFor<OuterViewModel>));
             dependencyResolver.Register(() => new InnerView(), typeof(IViewFor<InnerViewModel>));
+
+            // TODO : Figure out if this needs to actually be here.
+            //dependencyResolver.Register(() => new MainControllerWindow(), typeof(IViewFor<AppViewModel>));
 
             // Services + Misc
             dependencyResolver.Register(() => new DbContextScopeFactory(), typeof(IDbContextScopeFactory));

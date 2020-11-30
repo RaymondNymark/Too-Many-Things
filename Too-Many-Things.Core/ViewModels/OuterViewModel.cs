bc497c30a2bfc1c883276bd7b711427559e08572
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-30 13:24:37 +0100

    Tidy up OuterViewModel and OuterView.xaml

--- Too-Many-Things.Core/ViewModels/OuterViewModel.cs
+++ Too-Many-Things.Core/ViewModels/OuterViewModel.cs
@@ -9,40 +9,19 @@ using ReactiveUI;
 
 namespace Too_Many_Things.Core.ViewModels
 {
-    public interface IOuterViewModel : IRoutableViewModel
+    public class OuterViewModel : ReactiveObject
     {
-
-    }
-    public class OuterViewModel : ReactiveObject, IOuterViewModel
-    {
-        public string UrlPathSegment
-        {
-            get => "outer";
-        }
-        public IScreen HostScreen { get; protected set; }
-
         private IChecklistService _checklistService;
         private Checklist _selectedChecklist;
 
         public OuterViewModel(IScreen hostScreen, IChecklistService checklistService)
         {
-            HostScreen = hostScreen;
             _checklistService = checklistService;
+
+
         }
 
-        #region Data-Binding properties
 
-        // Keeps track of which checklist is 'selected' or 'clicked' or 'focused'.
-        public Checklist SelectedChecklist
-        {
-            get => _selectedChecklist;
-            set => this.RaiseAndSetIfChanged(ref _selectedChecklist, value);
-        }
 
-        public ObservableCollection<Checklist> ItemSource
-        {
-            get => _checklistService.GetLocalCollectionSource();
-        }
-        #endregion
     }
 }

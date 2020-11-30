b4d85236b942b6ff0b3b6f5c89856f55393ab065
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-11-30 13:22:06 +0100

    Create AppViewModel to act as defaultVM

---
+++ Too-Many-Things.Core/ViewModels/AppViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+
+namespace Too_Many_Things.Core.ViewModels
+{
+    public class AppViewModel : ReactiveObject, IScreen
+    {
+        public RoutingState Router { get; private set; }
+
+        public AppViewModel(RoutingState router = null)
+        {
+            Router = router ?? new RoutingState();
+
+            // All other startup tasks go here:
+
+
+
+            // Router.Navigate.Execute(new anyViewModel()) to fire off first default viewModel.
+            OpenView = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new ChecklistBagViewModel()));
+        }
+
+        // All commands should go here.
+        public ReactiveCommand<Unit, IRoutableViewModel> OpenView { get; }
+    }
+}

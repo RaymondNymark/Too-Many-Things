d516bfb4c87ee0449a5c386a27468388232b213a
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-02 06:24:15 +0100

    Implement handful of properties to ChecklistBagViewModel.cs

--- Too-Many-Things.Core/ViewModels/ChecklistBagViewModel.cs
+++ Too-Many-Things.Core/ViewModels/ChecklistBagViewModel.cs
@@ -7,6 +7,8 @@ using Too_Many_Things.Core.ViewModels;
 using System.Collections.ObjectModel;
 using ReactiveUI;
 using Splat;
+using DynamicData;
+using DynamicData.Binding;
 
 namespace Too_Many_Things.Core.ViewModels
 {
@@ -16,9 +18,28 @@ namespace Too_Many_Things.Core.ViewModels
         public IScreen HostScreen { get; }
         private IChecklistService _checklistService;
 
-        public ChecklistBagViewModel(IChecklistService checklistService = null)
+        // List of checklists that should be bound to.
+        private IObservableList<Checklist> _checklistList;
+        public IObservableList<Checklist> ChecklistList
         {
+            get => _checklistList;
+            set => this.RaiseAndSetIfChanged(ref _checklistList, value);
+        }
+
+        public Checklist SelectedChecklist;
+
+        public ChecklistBagViewModel(IScreen screen = null, IChecklistService checklistService = null)
+        {
+            HostScreen = screen ?? Locator.Current.GetService<IScreen>();
             _checklistService = checklistService ?? Locator.Current.GetService<IChecklistService>();
+
+            // Makes Selected checklist into an observable property.
+            IObservable<Checklist> selectedChecklist = this.WhenAnyValue(x => x.SelectedChecklist);
+
+            // TODO : Is this pointless?
+            var baseList = _checklistService.GetLocalCollectionSource();
+            _checklistList = baseList.ToObservableChangeSet()
+                .AsObservableList();
         }
     }
 }

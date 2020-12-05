9034d926e6aadef3f3a1ad2e74c51a960b1e758e
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-05 07:35:50 +0100

    Expand Checklist Model to implement a feature in an odd way.  TODO : Revert this with a better fix.

---
+++ Too-Many-Things.Core/Models/ChecklistPlus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Too_Many_Things.Core.Models
+{
+    public partial class Checklist
+    {
+        // TODO : Get rid of this horribleness without modifying model and fix
+        // template problem.  Also, this absolutely will not update reactively.
+        public int EntryCount
+        {
+            get => this.Entry.Count;
+        }
+
+        public string EntryCountString
+        {
+            get => $"There are {EntryCount} entries here.";
+        }
+    }
+}

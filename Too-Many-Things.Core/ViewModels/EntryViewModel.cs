8026890158f5b927b6a84ac7a9cb5bee43a97d8c
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-07 13:09:32 +0100

    Create EntryViewModel.cs

---
+++ Too-Many-Things.Core/ViewModels/EntryViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Too_Many_Things.Core.Services;
+using Too_Many_Things.Core.Models;
+using Too_Many_Things.Core.ViewModels;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using Splat;
+using DynamicData;
+using DynamicData.Binding;
+using System.Reactive;
+using System.Diagnostics;
+namespace Too_Many_Things.Core.ViewModels
+{
+    public interface IEntryViewModel
+    {
+
+    }
+    public class EntryViewModel : ReactiveObject, IRoutableViewModel, IEntryViewModel
+    {
+        public string UrlPathSegment => "EntryBag";
+        public IScreen HostScreen { get; }
+        private IChecklistService _checklistService;
+
+        // ItemCollection to bind to.
+        private ObservableCollection<Entry> _entryList;
+        public ObservableCollection<Entry> EntryListBindable
+        {
+            get => _entryList;
+            set => this.RaiseAndSetIfChanged(ref _entryList, value);
+        }
+
+        public EntryViewModel(Checklist checklist, IScreen screen = null, IChecklistService checklistService = null)
+        {
+           HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+           _checklistService = checklistService ?? Locator.Current.GetService<IChecklistService>();
+
+            _entryList = checklist.Entry;
+        }
+    }
+}

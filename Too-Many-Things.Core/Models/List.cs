0340159eac90e69e0b282b83c67f1d8d2df0b93f
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-10 08:18:56 +0100

    Create new List.cs and Entry.cs model classes

--- Too-Many-Things.Core/Models/List.cs
+++ Too-Many-Things.Core/Models/List.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 
 namespace Too_Many_Things.Core.Models
 {
-    public partial class Entry
+    public partial class List
     {
-        public int EntryID { get; set; }
+        public int ListID { get; set; }
         public string Name { get; set; }
-        public bool IsChecked { get; set; }
         public bool IsDeleted { get; set; }
         public int? SortOrder { get; set; }
 
-        public virtual List List { get; set; }
+        public virtual ICollection<Entry> Entries { get; set; }
     }
 }

77e00ca69d515ccb789e2c97bb39d83fcba4e056
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-10 14:29:24 +0100

    Create ChecklistContext.cs

---
+++ Too-Many-Things.Core/DbContext/ChecklistContext.cs
@@ -0,0 +1,41 @@
+﻿//using Too_Many_Things.Core.Models;
+//using Microsoft.EntityFrameworkCore;
+//using System.Data;
+
+//namespace Too_Many_Things.Core.DbContext
+//{
+//    public class ChecklistContext : Microsoft.EntityFrameworkCore.DbContext
+//    {
+//        // TODO : Absolutely get rid of this.
+//        public static readonly string DefaultConnection = "Server=(localdb)\\MSSQLLocalDB;Database=TooManyThings;Trusted_Connection=True;";
+
+
+//        public ChecklistContext(DbContextOptions<ChecklistContext> options = DefaultConnection) : base(options)
+//        {
+//        }
+
+//        public DbSet<List> Lists { get; set; }
+//        public DbSet<Entry> Entries { get; set; }
+
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            // Overriding default pluralization of names to keep it to singular.
+//            modelBuilder.Entity<List>().ToTable("List");
+//            modelBuilder.Entity<Entry>().ToTable("Entry");
+
+
+//            //modelBuilder.Entity<List>()
+//            //    .HasMany(c => c.Entries)
+//            //    .WithOne(e => e.List);
+//        }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning TODO : REMOVE THIS CONNECTION STRING
+//                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TooManyThings;Trusted_Connection=True;");
+//            }
+//        }
+//    }
+//}

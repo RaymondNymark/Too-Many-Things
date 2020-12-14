7a90919375e147e8e2531ec70af8178776b4cf64
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-14 07:39:44 +0100

    Create Model Entry.cs and List.cs for DB DataAccess

---
+++ Too-Many-Things.Core/DataAccess/Models/Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Too_Many_Things.Core.DataAccess.Models
+{
+    public partial class Entry
+    {
+        public int EntryID { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        public bool IsChecked { get; set; }
+        public bool IsDeleted { get; set; }
+        public int SortOrder { get; set; }
+
+        [Required]
+        public int ListID { get; set; }
+        [Required]
+        public virtual List List { get; set; }
+    }
+}
---
+++ Too-Many-Things.Core/DataAccess/Models/List.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Too_Many_Things.Core.DataAccess.Models
+{
+    public partial class List
+    {
+        public int ListID { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        public int? SortOrder { get; set; }
+
+        public virtual ObservableCollection<Entry> Entries { get; set; } = new ObservableCollection<Entry>();
+    }
+}

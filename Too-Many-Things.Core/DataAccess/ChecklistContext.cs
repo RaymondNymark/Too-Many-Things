1db25b9b4cadd354c3364620261337f64f4afd35
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-14 07:39:48 +0100

    Create ChecklistContext.cs

---
+++ Too-Many-Things.Core/DataAccess/ChecklistContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Configuration;
+using Too_Many_Things.Core.DataAccess.Models;
+
+namespace Too_Many_Things.Core.DataAccess
+{
+    public class ChecklistContext : DbContext
+    {
+        public ChecklistContext()
+        {
+
+        }
+
+        public ChecklistContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<List> Lists { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Lazy way of configuring DbContext.
+            if (!optionsBuilder.IsConfigured)
+            {
+                // TODO : Store connection string in a safe place and let user
+                // change it.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TooManyThingsDB;Trusted_Connection=True;");
+            }
+        }
+    }
+}

8c059312a24c90a3d1937407112c53d994c7e230
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-14 14:44:09 +0100

    Create SettingsView + SettingsView.xaml.cs

---
+++ Too-Many-Things.Wpf/Views/SettingsView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Too_Many_Things.Core.ViewModels;
+
+namespace Too_Many_Things.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : ReactiveWindow<SettingsViewModel>
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+            ViewModel = new SettingsViewModel();
+            // TODO : this.
+
+            this.WhenActivated(disposables =>
+            {
+
+            });
+        }
+    }
+}

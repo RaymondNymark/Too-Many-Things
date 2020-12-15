f0045e291baafe3a548a9b47f4485b931c407673
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-15 07:03:44 +0100

    Update SettingsViewModel.cs

--- Too-Many-Things.Core/ViewModels/SettingsViewModel.cs
+++ Too-Many-Things.Core/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Too_Many_Things.Core.ViewModels
@@ -12,9 +13,6 @@ namespace Too_Many_Things.Core.ViewModels
         public SettingsViewModel(RoutingState router = null)
         {
             Router = router ?? new RoutingState();
-
-            // Router.Navigate.Execute(new anyViewModel()) to fire off first default viewModel.
-            //OpenView = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new ChecklistBagViewModel()));
         }
     }
 }

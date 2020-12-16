8d1438a8b7fd58ae5ce8d896fe858a759e215a60
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-16 14:07:30 +0100

    Create basic ConnectionStringManager.cs
    This class manages connectionStrings by saving them in a txt file.  Because these are private connection strings, storing them in an encrypted way or using a webserver is not particularly useful😅

---
+++ Too-Many-Things.Core/Services/ConnectionStringManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Too_Many_Things.Core.Services
+{
+    /// <summary>
+    /// This class saves and retrieves ConnectionString saved on a txt file.
+    /// Because this is not a public connectionString, encrypting it or storing
+    /// it safely is not needed.
+    /// </summary>
+    public static class ConnectionStringManager
+    {
+        public static void SetConnectionString(string connectionString)
+        {
+            using (StreamWriter file = File.CreateText("ConnectionString.txt"))
+            {
+                file.Write(connectionString);
+            }
+        }
+
+        public static string GetConnectionString()
+        {
+            using (StreamReader file = File.OpenText("ConnectionString.txt"))
+            {
+                var result = file.ReadToEnd();
+                return result;
+            }
+        }
+    }
+}

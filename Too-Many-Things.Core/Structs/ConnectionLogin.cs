36a6ce8fdeb1c74a99d7cd2b7b95f1579194a15c
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-16 14:10:12 +0100

    Create ConnectionLogin struct to make handful of strings into a single object for convinience

---
+++ Too-Many-Things.Core/Structs/ConnectionLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Too_Many_Things.Core.DataAccess.Structs
+{
+    public struct ConnectionLogin
+    {
+        public string ServerName { get; }
+        public string DatabaseName { get; }
+        public string UserName { get; }
+        public string Password { get; }
+
+        public ConnectionLogin(string serverName, string databaseName, string userName = null, string password = null)
+        {
+            ServerName = serverName;
+            DatabaseName = databaseName;
+            UserName = userName;
+            Password = password;
+        }
+    }
+}

bf4ae33b1201cfcacd93feabf16210f2fc4c4025
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-16 09:09:13 +0100

    Create DataAccessService.cs

---
+++ Too-Many-Things.Core/Services/DataAccessService.cs
@@ -0,0 +1,15 @@
+﻿using Splat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Too_Many_Things.Core.Services
+{
+    public partial class DataAccessService : IEnableLogger
+    {
+        public DataAccessService()
+        {
+
+        }
+    }
+}

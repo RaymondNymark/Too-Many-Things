bddbfeedbc1b353b0117f460d2d985616dd965e9
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-17 07:15:11 +0100

    Update ConnectionStringManager with Async methods

--- Too-Many-Things.Core/Services/ConnectionStringManager.cs
+++ Too-Many-Things.Core/Services/ConnectionStringManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Too_Many_Things.Core.Services
 {
@@ -27,6 +28,17 @@ namespace Too_Many_Things.Core.Services
                 var result = file.ReadToEnd();
                 return result;
             }
+        }
+
+        public static async Task SetConnectionStringAsync(string connectionString)
+        {
+            await File.WriteAllTextAsync("ConnectionString.txt", connectionString);
+        }
+
+        public static async Task<string> GetConnectionStringAsync()
+        {
+            var connectionString = await File.ReadAllTextAsync("ConnectionString.txt");
+            return connectionString;
         }
     }
 }

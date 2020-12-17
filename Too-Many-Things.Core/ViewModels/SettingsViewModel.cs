141f20b96a5b0f2b699e8e0b8139fa6a5e264b91
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-17 14:46:29 +0100

    Implement methods and status message to be able to Test connection to see if it's valid.

--- Too-Many-Things.Core/ViewModels/SettingsViewModel.cs
+++ Too-Many-Things.Core/ViewModels/SettingsViewModel.cs
@@ -3,9 +3,11 @@ using Splat;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Too_Many_Things.Core.DataAccess.Structs;
 using Too_Many_Things.Core.Services;
 
@@ -15,6 +17,7 @@ namespace Too_Many_Things.Core.ViewModels
     {
         public RoutingState Router { get; private set; }
         private IDBConnectionService _dbConectionService;
+        public ReactiveCommand<Unit, Unit> TestConnectionCommand;
 
         public SettingsViewModel(RoutingState router = null, IDBConnectionService DBConnectionService = null)
         {
@@ -27,33 +30,89 @@ namespace Too_Many_Things.Core.ViewModels
                 .DistinctUntilChanged()
                 .Subscribe(x => UpdateConnectionStrings(ConnectionLogin));
 
+            var testConnectionCanExecute = this.WhenAnyValue(
+                x => x.ConnectionLogin.ServerName,
+                x => x.ConnectionLogin.DatabaseName,
+                (serverName, databaseName) =>
+                    !string.IsNullOrEmpty(serverName) &&
+                    !string.IsNullOrEmpty(databaseName));
+
+            TestConnectionCommand = ReactiveCommand.CreateFromTask(PingConnectionStringAsync, testConnectionCanExecute);
+
             // Debug-only TODO : Remove this.
             //this.WhenAnyValue(x => x.ConnectionStrings).Subscribe(x => Debug.WriteLine($"{ConnectionStrings.pingConnectionString} and {ConnectionStrings.connectionString}"));
         }
 
         #region Properties
         private ConnectionLogin _connectionLogin;
-        private (string pingConnectionString, string connectionString) _connectionStrings; 
+        private (string pingConnectionString, string connectionString) _connectionStrings;
+        private string _connectionStatus;
+        private Brush _connectionStatusBrush;
 
         public ConnectionLogin ConnectionLogin
         {
             get => _connectionLogin;
             set => this.RaiseAndSetIfChanged(ref _connectionLogin, value);
         }
-
         public (string pingConnectionString, string connectionString) ConnectionStrings
         {
             get => _connectionStrings;
             set => this.RaiseAndSetIfChanged(ref _connectionStrings, value);
         }
+
+        public string ConnectionStatus
+        {
+            get => _connectionStatus;
+            set => this.RaiseAndSetIfChanged(ref _connectionStatus, value);
+        }
+
+        public Brush ConnectionStatusBrush
+        {
+            get => _connectionStatusBrush;
+            set => this.RaiseAndSetIfChanged(ref _connectionStatusBrush, value);
+        }
         #endregion
 
+
+        #region Asynchronous Tasks & Methods
         private void UpdateConnectionStrings(ConnectionLogin connectionLogin)
         {
             ConnectionStrings = DBConnectionService.CreateConnectionString(connectionLogin);
         }
 
-        #region Asynchronous Tasks
+        // For color of connection status.
+        public Brush GetColor()
+        {
+            switch (ConnectionStatus)
+            {
+                case "Successfully connected to the server.":
+                    return Brushes.Green;
+                case "Failed to connect. Please try again.":
+                    return Brushes.Red;
+                default:
+                    return Brushes.Black;
+            }
+        }
+ 
+        private async Task PingConnectionStringAsync()
+        {
+            ConnectionStatus = "Attempting to establish a connection...";
+            ConnectionStatusBrush = GetColor();
+
+            var status = await DBConnectionService.IsServerConnectedAsync(ConnectionStrings.pingConnectionString);
+
+            if (status == true)
+            {
+                ConnectionStatus = "Successfully connected to the server.";
+                ConnectionStatusBrush = GetColor();
+            }
+            else
+            {
+                ConnectionStatus = "Failed to connect. Please try again.";
+                ConnectionStatusBrush = GetColor();
+            }
+        }
+
         //private async Task<bool> TestConnectionAsync()
         //{
         //    var connectionLogin = new ConnectionLogin(ServerName, DatabaseName, UserName, Password);

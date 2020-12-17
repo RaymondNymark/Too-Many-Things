a291f92184a52173a00acf82b082212fa168d912
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-17 06:38:57 +0100

    Update DataAccessService.cs

--- Too-Many-Things.Core/Services/DataAccessService.cs
+++ Too-Many-Things.Core/Services/DataAccessService.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Too_Many_Things.Core.Services
 {
-    public partial class DataAccessService : IEnableLogger
+    public class DataAccessService : IEnableLogger
     {
         public DataAccessService()
         {

e97dbed8d6b8a7438eacb94ed33e0b571e8b176e
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-19 07:02:33 +0100

    Create initial properties and InitializeApp Method

--- Too-Many-Things.Core/ViewModels/PrimaryViewModel.cs
+++ Too-Many-Things.Core/ViewModels/PrimaryViewModel.cs
@@ -11,6 +11,7 @@ using DynamicData;
 using DynamicData.Binding;
 using System.Reactive;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Too_Many_Things.Core.ViewModels
 {
@@ -18,10 +19,49 @@ namespace Too_Many_Things.Core.ViewModels
     {
         public string UrlPathSegment => "Primary";
         public IScreen HostScreen { get; }
+        public ReactiveCommand<Unit, Unit> RefreshCommand { get; }
 
         public PrimaryViewModel(IScreen screen = null, IChecklistService checklistService = null)
         {
             HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+            RefreshCommand = ReactiveCommand.Create(()=> InitializeApp());
+
+            InitializeApp();
         }
+
+        #region Properties
+        private string _configurationStatus;
+        public string ConfigurationStatus
+        {
+            get => _configurationStatus;
+            set => this.RaiseAndSetIfChanged(ref _configurationStatus, value);
+        }
+
+        #endregion
+
+        #region Asynchronous Tasks & Methods
+        /// <summary>
+        /// Sets up the primary view to be able to be used if a database
+        /// connection has been configured. Otherwise it asks user to configure
+        /// it.
+        /// </summary>
+        private void InitializeApp()
+        {
+            var isConfigured = ConnectionStringManager.ConnectionIsConfigured();
+
+            if (isConfigured)
+            {
+                // Big guns
+                // Bring DB data into a collection<t> from service and do things. 
+            }
+            else
+            {
+                // Small guns
+                // Changes configuration Status text to inform user that the
+                // database hasn't been initialized.
+                ConfigurationStatus = "A database has not been configured. Please configure one in the settings. :)";
+            }
+        }
+        #endregion
     }
 }

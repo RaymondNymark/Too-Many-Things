0bff1f02c65b90ee7094b8e29b430424efee3c9b
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-19 13:09:33 +0100

    Create ChecklistDataService.cs

---
+++ Too-Many-Things.Core/Services/ChecklistDataService.cs
@@ -0,0 +1,43 @@
+﻿using DynamicData;
+using Microsoft.EntityFrameworkCore;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Too_Many_Things.Core.DataAccess;
+using Too_Many_Things.Core.DataAccess.Models;
+
+namespace Too_Many_Things.Core.Services
+{
+    public partial class ChecklistDataService : IEnableLogger
+    {
+        private IChecklistContextFactory _checklistContextFactory;
+
+        public ChecklistDataService(IChecklistContextFactory checklistContextFactory = null)
+        {
+            _checklistContextFactory = checklistContextFactory ?? Locator.Current.GetService<IChecklistContextFactory>();
+        }
+
+        // Kind of a messy and inelegant solution. Wonder if there's a better way to do this.
+        public List<List> LoadData()
+        {
+            using (var context = _checklistContextFactory.CreateDbContext())
+            {
+                var checklists = context.Lists
+                    .Include(e => e.Entries)
+                    .ToList();
+
+                return checklists;
+            }
+        }
+
+        public ObservableCollection<List> LocalSource
+        {
+            get => _checklistContextFactory.CreateDbContext().Lists.Local.ToObservableCollection();
+        }
+    }
+}

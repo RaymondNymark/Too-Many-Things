6a4f2f9251be2e1830509be8ea5b65c83c377c6d
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-22 08:08:23 +0100

    Create new ListTemplate userControl

---
+++ Too-Many-Things.Wpf/Templates/ListTemplate.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using Too_Many_Things.Core.DataAccess.Models;
+
+namespace Too_Many_Things.Wpf.Templates
+{
+    /// <summary>
+    /// Interaction logic for ListTemplate.xaml
+    /// </summary>
+    public partial class ListTemplate : ReactiveUserControl<List>
+    {
+        public ListTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}

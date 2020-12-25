5388bbb8b64742d37a74bc5c178eeb0ce7a75386
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-25 07:22:37 +0100

    Create Entry + List ViewModels for data binding

--- Too-Many-Things.Core/ViewModels/EntryViewModel.cs
+++ Too-Many-Things.Core/ViewModels/EntryViewModel.cs
@@ -1,53 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Too_Many_Things.Core.Services;
-using Too_Many_Things.Core.ViewModels;
-using System.Collections.ObjectModel;
-using ReactiveUI;
-using Splat;
-using DynamicData;
-using DynamicData.Binding;
-using System.Reactive;
-using System.Diagnostics;
-using ReactiveUI.Fody.Helpers;
-using Too_Many_Things.Core.DataAccess.Models;
 
 namespace Too_Many_Things.Core.ViewModels
 {
-    public interface IEntryViewModel
+    public class EntryViewModel
     {
-        ReactiveCommand<Unit, IRoutableViewModel> CloseChecklist { get; }
-        ObservableCollection<Entry> EntryList { get; }
-    }
-    public class EntryViewModel : ReactiveObject, IRoutableViewModel, IEntryViewModel
-    {
-        #region Properties
-        public string UrlPathSegment => "Entries";
-        public IScreen HostScreen { get; }
-        private ChecklistDataService _checklistService;
+        public int EntryID { get; set; }
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+        public bool IsDeleted { get; set; }
+        public int SortOrder { get; set; }
+        public int ListID { get; set; }
 
-        // Collection syphon from entries..
-        [Reactive]
-        public ObservableCollection<Entry> EntryList { get; set; }
-        // Flag to enable / disable edit mode.
-        [Reactive]
-        public bool EditModeEnabled { get; set; }
-
-        #endregion
-
-        #region Command-region
-        public ReactiveCommand<Unit, IRoutableViewModel> CloseChecklist { get; }
-        #endregion
-
-        public EntryViewModel(List selectedChecklist, IScreen screen = null, ChecklistDataService checklistService = null)
+        public EntryViewModel(int entryID, string name, bool isChecked, bool isDeleted, int sortOrder, int listID)
         {
-           HostScreen = screen ?? Locator.Current.GetService<IScreen>();
-           _checklistService = checklistService ?? Locator.Current.GetService<ChecklistDataService>();
-
-            EntryList = selectedChecklist.Entries;
-
-            CloseChecklist = ReactiveCommand.CreateFromObservable(() => HostScreen.Router.Navigate.Execute(new PrimaryViewModel(HostScreen, null)));
-        }   
+            EntryID = entryID;
+            Name = name;
+            IsChecked = isChecked;
+            IsDeleted = isDeleted;
+            SortOrder = sortOrder;
+            ListID = listID;
+        }
     }
 }
---
+++ Too-Many-Things.Core/ViewModels/ListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using Too_Many_Things.Core.DataAccess.Models;
+
+namespace Too_Many_Things.Core.ViewModels
+{
+    public class ListViewModel
+    {
+        public int ListID { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public int? SortOrder { get; set; }
+        public virtual ObservableCollection<Entry> Entries { get; set; }
+
+        public ListViewModel(int listID, string name, bool isDeleted, int? sortOrder, ObservableCollection<Entry> entries)
+        {
+            ListID = listID;
+            Name = name;
+            IsDeleted = isDeleted;
+            SortOrder = sortOrder;
+            Entries = entries;
+        }
+    }
+}

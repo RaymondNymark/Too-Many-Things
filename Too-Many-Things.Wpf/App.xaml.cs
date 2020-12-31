83a02d7875fcc6d3b73c1553b06f6a4dfbe5cbc5
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-12-31 09:26:33 +0100

    Update App.xaml.cs with a way to change themes by adding and clearing resource dictionaries with using a method

--- Too-Many-Things.Wpf/App.xaml.cs
+++ Too-Many-Things.Wpf/App.xaml.cs
@@ -5,11 +5,53 @@ using System.Windows;
 
 namespace Too_Many_Things.Wpf
 {
+    public enum Theme { Light, Dark};
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public static Theme Theme { get; set; } = Theme.Dark;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ChangeTheme(Theme.Dark);
+        }
+
+        public void ChangeTheme(Theme newTheme)
+        {
+            Theme = newTheme;
+            Resources.Clear();
+            Resources.MergedDictionaries.Clear();
+
+            if (Theme == Theme.Dark)
+                ApplyDarkTheme();
+            else if (Theme == Theme.Light)
+                ApplyLightTheme();
+
+            ApplySharedResources();
+        }
+
+        private void ApplyDarkTheme()
+        {
+
+        }
+
+        private void ApplyLightTheme()
+        {
+
+        }
+
+        private void ApplySharedResources()
+        {
+            AddResourceDictionary("Themes\\Shared.xaml");
+        }
         
+        private void AddResourceDictionary(string src)
+        {
+            Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new System.Uri(src, System.UriKind.Relative) });
+        }
     }
 }

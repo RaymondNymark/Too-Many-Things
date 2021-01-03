c097ea722908f040ab474b269905da67a924e213
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-01-03 13:56:05 +0100

    Create a custom windows title to be able to customize colors so it matches to the theme

--- Too-Many-Things.Wpf/MainControllerWindow.xaml.cs
+++ Too-Many-Things.Wpf/MainControllerWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using ReactiveUI;
+using System;
 using System.Reactive.Disposables;
+using System.Windows;
+using System.Windows.Threading;
 using Too_Many_Things.Core.ViewModels;
 
 namespace Too_Many_Things.Wpf
@@ -28,5 +31,25 @@ namespace Too_Many_Things.Wpf
                     .DisposeWith(disposables);
             });
         }
+
+        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            // This is a hacky solution to have an animation when a window is
+            // minimized.  It changes WindowStyle to SingleBorderWindow and
+            // changes back to none once it's been opened again.
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.WindowState = WindowState.Minimized;
+        }
+        private void MainWindow_OnActivated(object sender, EventArgs e)
+        {
+            //change the WindowStyle back to None, but only after the Window has been activated
+            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => WindowStyle = WindowStyle.None));
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }

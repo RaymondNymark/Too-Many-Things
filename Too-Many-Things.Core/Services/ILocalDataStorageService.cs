4da14457a29fe1d457d14dca3487c07b565b4544
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-01-12 11:17:40 +0100

    Create new LocalDataStorageService class in order to add functionality to store checklist data localy as opposed to only storing on an SQL-database and add the Newtonsoft.Json nuget package
    Currently the application can only show checklist data if it's stored on a local/online SQL database.  This adds new LocalDataStorage class that enables storing local data as .json file by using the Newtonsoft.Json nuget package.

---
+++ Too-Many-Things.Core/Services/ILocalDataStorageService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Too_Many_Things.Core.DataAccess.Models;
+
+namespace Too_Many_Things.Core.Services
+{
+    public interface ILocalDataStorageService
+    {
+        Task<ObservableCollection<List>> RetrieveStoredObjectAsync();
+        Task StoreObject(object objectToStore);
+    }
+}
---
+++ Too-Many-Things.Core/Services/LocalDataStorageService.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using Too_Many_Things.Core.DataAccess.Models;
+
+namespace Too_Many_Things.Core.Services
+{
+    public class LocalDataStorageService : ILocalDataStorageService
+    {
+        private string _path { get; set; }
+        private string _filePath { get; set; }
+
+        public LocalDataStorageService()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Starts the process of creating the means of storing data locally on
+        /// the user's computer as opposed to an online database.
+        /// </summary>
+        private void Initialize()
+        {
+            // Check if Too-Many-Things folder exists in roaming app data and
+            // create if it does not.
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            _path = appDataPath + @"\Too-Many-Things";
+            _filePath = _path + @"\localChecklistData.json";
+
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+
+            // Check if localChecklistData.json file exists and creates if it doesn't.
+            if (!File.Exists(_filePath))
+            {
+                File.CreateText(_filePath);
+            }
+        }
+
+        /// <summary>
+        /// Stores an object into the localChecklistData.json file to be able to
+        /// be retrieved between sessions.
+        /// </summary>
+        /// <param name="objectToStore">Object to store</param>
+        public async Task StoreObject(object objectToStore)
+        {
+            using (StreamWriter file = new StreamWriter(_filePath))
+            {
+                // Serializing the object into localChecklistData.json.
+                await file.WriteLineAsync(JsonConvert.SerializeObject(objectToStore, Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }));
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the stored object in localChecklistData.json. Returns null if it's empty.
+        /// </summary>
+        /// <returns>The stored object</returns>
+        public async Task<ObservableCollection<List>> RetrieveStoredObjectAsync()
+        {
+            ObservableCollection<List> result;
+
+            using (StreamReader file = new StreamReader(_filePath))
+            {
+                // De-serializing the data back into an observableCollection.
+                result = JsonConvert.DeserializeObject<ObservableCollection<List>>(await file.ReadToEndAsync());
+            }
+
+            return result;
+        }
+    }
+}
